8ef0fb1653b08b02da0c37c6f586de4b1ee70c2f
Drenbb <boldin_0202@mail.ru>
2021-10-31 23:09:18 +0300

    Пагинация и фильтры

---
+++ pages/pgDLL.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DLLCSharp;
+
+namespace WpfApp.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для pgDLL.xaml
+    /// </summary>
+    public partial class pgDLL : Page
+    {
+        List<users> users;
+        public pgDLL()
+        {
+            InitializeComponent();
+            users = BaseConnect.BaseModel.users.ToList();
+        }
+
+        private void btnAverage_Click(object sender, RoutedEventArgs e)
+        {
+            List<DateTime> dt = new List<DateTime>();
+            foreach (users u in users)
+            {
+                dt.Add(u.dr);
+            }
+            MessageBox.Show("Средний возраст пользователей" + Class1.averageDate(dt));
+
+        }
+
+        private void btnFindName_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = null; 
+            List<string> names = new List<string>();
+            foreach (users u in users)
+            {
+                names.Add(u.name);
+            }
+            foreach (string s in Class1.findName(names, tbPathName.Text))
+            {
+                tbOutput.Text += s + "\n";
+            }
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPages.MainFrame.GoBack();
+        }
+    }
+}
--- pages/pgUsersToList.xaml.cs
+++ pages/pgUsersToList.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DLLCSharp;
 
 namespace WpfApp.pages
 {
@@ -21,11 +22,14 @@ namespace WpfApp.pages
     public partial class pgUsersToList : Page
     {
         List<users> users;
+        List<users> listUsers;
         public pgUsersToList()
         {
             InitializeComponent();
+            spPages.Visibility = Visibility.Hidden;
             users = BaseConnect.BaseModel.users.ToList();
             lbUsers.ItemsSource = users;
+            listUsers = users;
             List<genders> genders = BaseConnect.BaseModel.genders.ToList();
             cbGenderS.ItemsSource = genders;
             cbGenderS.SelectedValuePath = "id";
@@ -49,37 +53,35 @@ namespace WpfApp.pages
             LoadPages.MainFrame.Navigate(new ChangeUser(CurrentUser));
         }
 
-        private void btnSort_Click(object sender, RoutedEventArgs e)
+        private void Sort(object sender, RoutedEventArgs e)
         {
-            List<users> listUsers = users.ToList();
-            if (tbStart.Text != "" && tbFinish.Text != "")
+            //MessageBox.Show(listUsers.Count.ToString());
+            try
             {
-                int start = Convert.ToInt32(tbStart.Text) - 1;
-                int finish = Convert.ToInt32(tbFinish.Text);
-                listUsers = users.Skip(start).Take(finish - start).ToList();
+                if (tbStart.Text != "" && tbFinish.Text != "")
+                {
+                    int start = Convert.ToInt32(tbStart.Text) - 1;
+                    int finish = Convert.ToInt32(tbFinish.Text);
+                    listUsers = users.Skip(start).Take(finish - start).ToList();
+                }
+                if (dpDate.SelectedDate != null)
+                    listUsers = listUsers.Where(x => x.dr == (DateTime)dpDate.SelectedDate).ToList();
+                if (cbGenderS.SelectedIndex != -1)
+                    listUsers = listUsers.Where(x => x.gender == Convert.ToInt32(cbGenderS.SelectedValue)).ToList();
+                if (tbInputName.Text != "")
+                    listUsers = listUsers.Where(x => x.name.Contains(tbInputName.Text)).ToList();
             }
-            else if (dpDate.SelectedDate != null && cbGenderS.SelectedItem == null)
-            {
-                listUsers = users.Where(x => x.dr == (DateTime)dpDate.SelectedDate).ToList();
-            }
-            else if (cbGenderS.SelectedItem != null && dpDate.SelectedDate == null)
-            {
-                listUsers = users.Where(x => x.gender == Convert.ToInt32(cbGenderS.SelectedValue)).ToList();
-            }
-            else if (dpDate.SelectedDate != null && cbGenderS.SelectedItem != null)
-            {
-                listUsers = users.Where(x => x.dr == (DateTime)dpDate.SelectedDate && x.gender == Convert.ToInt32(cbGenderS.SelectedValue)).ToList();
-            }
+            catch (Exception d) { MessageBox.Show(d.Message); }
             lbUsers.ItemsSource = listUsers;
         }
 
         private void btnRset_Click(object sender, RoutedEventArgs e)
         {
-            lbUsers.ItemsSource = users;
             cbGenderS.SelectedItem = null;
             dpDate.Text = null;
             tbStart.Text = null;
             tbFinish.Text = null;
+            lbUsers.ItemsSource = users;
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
@@ -103,6 +105,135 @@ namespace WpfApp.pages
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
             LoadPages.MainFrame.Navigate(new login());
+        }
+
+        private void btnDLL_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPages.MainFrame.Navigate(new pgDLL());
+        }
+
+
+
+        int currpg = 1;
+        private void GoPage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                TextBlock tb = (TextBlock)sender;
+                int countList = users.Count;
+                int countzap = Convert.ToInt32(txtPageCount.Text);
+                int countpage = countList / countzap;
+                txtNext.Visibility = Visibility.Visible;
+                txtPrev.Visibility = Visibility.Visible;
+                switch (tb.Uid)
+                {
+                    case "prev":
+                        currpg--;
+                        break;
+                    case "1":
+                        if (currpg < 3) currpg = 1;
+                        else if (currpg > countpage) currpg = countpage - 4;
+                        else currpg -= 2;
+                        break;
+                    case "2":
+                        if (currpg < 3) currpg = 2;
+                        else if (currpg > countpage) currpg = countpage - 3;
+                        else currpg--;
+                        break;
+                    case "3":
+                        if (currpg < 3) currpg = 3;
+                        else if (currpg > countpage) currpg = countpage - 2;
+                        break;
+                    case "4":
+                        if (currpg < 3) currpg = 4;
+                        else if (currpg > countpage) currpg = countpage - 1;
+                        else currpg++;
+                        break;
+                    case "5":
+                        if (currpg < 3) currpg = 5;
+                        else if (currpg > countpage) currpg = countpage;
+                        else currpg += 2;
+                        break;
+                    case "next":
+                        currpg++;
+                        break;
+                    default:
+                        currpg = 1;
+                        break;
+                }
+
+                //отрисовка
+                if (currpg < 3)
+                {
+                    txt1.Text = " 1 ";
+                    txt2.Text = " 2 ";
+                    txt3.Text = " 3 ";
+                    txt4.Text = " 4 ";
+                    txt5.Text = " 5 ";
+                }
+                else if (currpg > countpage - 2)
+                {
+                    txt1.Text = " " + (countpage - 4).ToString() + " ";
+                    txt2.Text = " " + (countpage - 3).ToString() + " ";
+                    txt3.Text = " " + (countpage - 2).ToString() + " ";
+                    txt4.Text = " " + (countpage - 1).ToString() + " ";
+                    txt5.Text = " " + (countpage).ToString() + " ";
+
+                }
+                else
+                {
+                    txt1.Text = " " + (currpg - 2).ToString() + " ";
+                    txt2.Text = " " + (currpg - 1).ToString() + " ";
+                    txt3.Text = " " + (currpg).ToString() + " ";
+                    txt4.Text = " " + (currpg + 1).ToString() + " ";
+                    txt5.Text = " " + (currpg + 2).ToString() + " ";
+
+                }
+                txtCurentPage.Text = "Текущая страница: " + (currpg).ToString();
+
+                if (currpg < 1)
+                {
+                    currpg = 1;
+                    txtPrev.Visibility = Visibility.Hidden;
+                }
+
+                if (currpg == countpage)
+                {
+                    currpg = countpage;
+                    txtNext.Visibility = Visibility.Hidden;
+                }
+
+                listUsers = users.Skip(currpg * countzap - countzap).Take(countzap).ToList();
+                lbUsers.ItemsSource = listUsers;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void txtPageCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (txtPageCount.Text == "")
+                {
+                    listUsers = users.ToList();
+                    spPages.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    listUsers = users.Take(Convert.ToInt32(txtPageCount.Text)).ToList();
+                    spPages.Visibility = Visibility.Visible;
+                }
+
+
+                lbUsers.ItemsSource = listUsers;
+            }
+            catch
+            {
+
+            }
         }
     }
 }
